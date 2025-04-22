88c3ff570f2eb69e40d2a568e4d3983f5a2cb6e6
Gaurav Upreti <ketchup-08-fang@icloud.com>
2025-04-22 11:05:09 +0530

    updated the API

--- app/apis/routes/users.py
+++ app/apis/routes/users.py
@@ -9,8 +9,8 @@ from app.apis.deps import get_current_user
 
 router = APIRouter()
 
-@router.get("/me", response_model=UserOut)
-def read_users_me(
+@router.get("/", response_model=UserOut)
+def get_user(
     current_user: User = Depends(get_current_user)
 ):
     return current_user
